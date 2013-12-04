c6db0a523c0b7d3852fa3025e343e12be7556442
Daniel Shorten <danielshorten@gmail.com>
2013-12-04 12:33:17 -0700

    Added missing import

--- omod/src/main/webapp/resources/js/view/editors.js
+++ omod/src/main/webapp/resources/js/view/editors.js
@@ -19,6 +19,7 @@ define(
 		openhmis.url.inventoryBase + 'js/model/item',
 		openhmis.url.inventoryBase + 'js/model/department',
         openhmis.url.inventoryBase + 'js/model/category',
+        openhmis.url.inventoryBase + 'js/view/search',
 		openhmis.url.backboneBase + 'js/lib/backbone-forms',
 		openhmis.url.backboneBase + 'js/lib/labelOver',
 		openhmis.url.backboneBase + 'js/view/editors'
