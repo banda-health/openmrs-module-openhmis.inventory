e32b942ff9ed47f842f5805088a8d052a70e72c4
Mario Somrei <github@gmx.de>
2014-04-17 10:36:09 +0300

    only show inventory page when user has role

--- omod/src/main/java/org/openmrs/module/openhmis/inventory/extension/html/InventoryLinkExt.java
+++ omod/src/main/java/org/openmrs/module/openhmis/inventory/extension/html/InventoryLinkExt.java
@@ -1,14 +1,34 @@
 package org.openmrs.module.openhmis.inventory.extension.html;
 
+import java.util.Map;
+
+import org.openmrs.User;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.openhmis.inventory.api.util.PrivilegeConstants;
 import org.openmrs.module.openhmis.inventory.web.ModuleWebConstants;
 import org.openmrs.module.web.extension.LinkExt;
 
 public class InventoryLinkExt extends LinkExt{
 
+    private boolean hasInventoryRights = false;
+
+    @Override
+    public void initialize(Map<String, String> parameterMap) {
+        super.initialize(parameterMap);
+        User currentUser = Context.getAuthenticatedUser();
+        if (currentUser != null) {
+            boolean hasViewItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.VIEW_ITEMS);
+            boolean hasManageItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.MANAGE_ITEMS);
+            boolean hasPurgeItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.PURGE_ITEMS);
+            if (hasViewItemsPrivilege || hasManageItemsPrivilege || hasPurgeItemsPrivilege) {
+                hasInventoryRights = true;
+            }
+        }
+    }
+
     @Override
     public String getLabel() {
-        if (true) {
+        if (hasInventoryRights) {
             return "openhmis.inventory.page";
         }
         return null;
