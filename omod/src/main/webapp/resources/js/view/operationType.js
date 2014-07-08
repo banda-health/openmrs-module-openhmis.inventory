289cd70552c25efea986326192a81911ca77cec4
Wes Brown <wes@weslandia.org>
2014-07-08 16:34:24 +0300

    Updated operation type view to use common view defined in backbone forms.

--- omod/src/main/webapp/resources/js/view/operationType.js
+++ omod/src/main/webapp/resources/js/view/operationType.js
@@ -1,46 +1,15 @@
 define(
 	[
 		openhmis.url.backboneBase + 'js/view/generic',
+        openhmis.url.backboneBase + 'js/view/openhmis',
 		openhmis.url.inventoryBase + 'js/model/operation',
 		openhmis.url.inventoryBase + 'js/model/stockroom',
 		'link!' + openhmis.url.inventoryBase + 'css/style.css'
 	],
 	function(openhmis) {
-		openhmis.OperationTypeEditView = openhmis.GenericAddEditView.extend({
+		openhmis.OperationTypeEditView = openhmis.CustomizableInstanceTypeAddEditView.extend({
 			tmplFile: openhmis.url.inventoryBase + 'template/operationType.html',
-			tmplSelector: '#detail-template',
-
-			prepareModelForm: function(model, options) {
-				var form = openhmis.GenericAddEditView.prototype.prepareModelForm.call(this, model, options);
-				form.on('attributeTypes:change', this.makeTypesSortable);
-				this.makeTypesSortable(form);
-				return form;
-			},
-
-			makeTypesSortable: function(form) {
-				form = form ? form : this.modelForm;
-				form.$('.bbf-list ul').sortable();
-			},
-
-			save: function() {
-				var attributes = this.$('.bbf-list ul').sortable("widget").children();
-				$(attributes).each(function() {
-					$(this).attr("id", "attr-" + $(attributes).index(this));
-				});
-				var items = this.modelForm.fields['attributeTypes'].editor.items;
-				for (var id in items) {
-					var getValue = items[id].getValue;
-					var newGetValue = function() {
-						var order = $(this.el).attr("id");
-						order = parseInt(order.substring(order.lastIndexOf('-') + 1));
-						var value = getValue.call(this);
-						value.attributeOrder = order;
-						return value;
-					}
-					items[id].getValue = newGetValue;
-				}
-				openhmis.GenericAddEditView.prototype.save.call(this);
-			}
+			tmplSelector: '#detail-template'
 		});
 
 		return openhmis;
