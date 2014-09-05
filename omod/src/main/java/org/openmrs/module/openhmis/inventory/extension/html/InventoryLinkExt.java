f323d859855484ad8450f024593389407ea26915
Mario Somrei <github@gmx.de>
2014-09-05 15:40:17 +0300

    chage privileges to view inventory page link

--- omod/src/main/java/org/openmrs/module/openhmis/inventory/extension/html/InventoryLinkExt.java
+++ omod/src/main/java/org/openmrs/module/openhmis/inventory/extension/html/InventoryLinkExt.java
@@ -1,37 +1,15 @@
 package org.openmrs.module.openhmis.inventory.extension.html;
 
-import org.openmrs.User;
-import org.openmrs.api.context.Context;
-import org.openmrs.module.openhmis.inventory.api.util.PrivilegeConstants;
 import org.openmrs.module.openhmis.inventory.web.ModuleWebConstants;
+import org.openmrs.module.openhmis.inventory.web.PrivilegeWebConstants;
 import org.openmrs.module.web.extension.LinkExt;
-
-import java.util.Map;
 
 public class InventoryLinkExt extends LinkExt {
 
-    private boolean hasInventoryRights = false;
-
-    @Override
-    public void initialize(Map<String, String> parameterMap) {
-        super.initialize(parameterMap);
-        User currentUser = Context.getAuthenticatedUser();
-        if (currentUser != null) {
-            boolean hasViewItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.VIEW_ITEMS);
-            boolean hasManageItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.MANAGE_ITEMS);
-            boolean hasPurgeItemsPrivilege = currentUser.hasPrivilege(PrivilegeConstants.PURGE_ITEMS);
-            if (hasViewItemsPrivilege || hasManageItemsPrivilege || hasPurgeItemsPrivilege) {
-                hasInventoryRights = true;
-            }
-        }
-    }
 
     @Override
     public String getLabel() {
-        if (hasInventoryRights) {
-            return "openhmis.inventory.page";
-        }
-        return null;
+    	return "openhmis.inventory.page";
     }
 
     @Override
@@ -41,7 +19,7 @@ public class InventoryLinkExt extends LinkExt {
 
     @Override
     public String getRequiredPrivilege() {
-        return PrivilegeConstants.MANAGE_ITEMS;
+        return PrivilegeWebConstants.INVENTORY_PAGE_PRIVILEGES;
     }
 
     public String getPortletUrl() {
