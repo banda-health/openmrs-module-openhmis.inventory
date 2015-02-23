2152e217ad6126eee66842226bf1c3b65c457df6
stephen morris kinyori <kinyorimorris@gmail.com>
2015-02-23 10:45:16 +0300

    Fixed indentation problem on the items.js script 		inv-63^fixed

--- omod/src/main/webapp/resources/js/screen/items.js
+++ omod/src/main/webapp/resources/js/screen/items.js
@@ -19,14 +19,13 @@ curl(
 		openhmis.url.backboneBase + 'js/lib/backbone-forms',
 		openhmis.url.inventoryBase + 'js/model/item',
 		openhmis.url.inventoryBase + 'js/model/department',
-        openhmis.url.inventoryBase + 'js/model/category',
+		openhmis.url.inventoryBase + 'js/model/category',
 		openhmis.url.backboneBase + 'js/view/generic',
-		//openhmis.url.backboneBase + 'js/view/list',
 		openhmis.url.backboneBase + 'js/view/editors',
 		openhmis.url.inventoryBase + 'js/view/editors',
 		openhmis.url.backboneBase + 'js/view/search',
 		openhmis.url.inventoryBase + 'js/view/search',
-        openhmis.url.inventoryBase + 'js/view/list',
+		openhmis.url.inventoryBase + 'js/view/list',
 		openhmis.url.inventoryBase + 'js/view/item'
 	],
 	function($, openhmis) {
