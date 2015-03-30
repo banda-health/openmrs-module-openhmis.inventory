1ae5393baaed7e97ea27f31ffc6860c698429578
stevekinyori <kinyorimorris@gmail.com>
2015-03-30 16:10:15 +0300

    Created the  Inventory stocktake resource to handle stoketake operations 	^inv-246 pending

--- omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeResource.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeResource.java
@@ -1,22 +1,39 @@
 package org.openmrs.module.webservices.rest.resource;
 
-import java.util.List;
-
+import org.openmrs.api.context.Context;
 import org.openmrs.module.openhmis.commons.api.entity.IMetadataDataService;
-import org.openmrs.module.openhmis.inventory.api.model.InventoryStockTake;
-import org.openmrs.module.openhmis.inventory.api.model.InventoryStockTakeEntity;
+import org.openmrs.module.openhmis.inventory.api.IStockOperationService;
+import org.openmrs.module.openhmis.inventory.api.IStockroomDataService;
+import org.openmrs.module.openhmis.inventory.api.WellKnownOperationTypes;
+import org.openmrs.module.openhmis.inventory.api.model.*;
 import org.openmrs.module.openhmis.inventory.web.ModuleRestConstants;
-import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
+import org.springframework.web.client.RestClientException;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Resource(name = ModuleRestConstants.INVENTORY_STOCK_TAKE_RESOURCE, supportedClass = InventoryStockTake.class,
-supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" })
-public class InventoryStockTakeResource extends BaseRestMetadataResource<InventoryStockTake> {
+		supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" }) public class InventoryStockTakeResource
+		extends BaseRestMetadataResource<InventoryStockTake> {
 
-	@Override
-	public DelegatingResourceDescription getRepresentationDescription(Representation rep)  {
+	private IStockOperationService operationService;
+	private IStockroomDataService stockroomDataService;
+
+	public InventoryStockTakeResource() {
+		this.stockroomDataService = Context.getService(IStockroomDataService.class);
+		this.operationService = Context.getService(IStockOperationService.class);
+	}
+
+	@Override public Class<? extends IMetadataDataService<InventoryStockTake>> getServiceClass() {
+		return null;
+	}
+
+	@Override public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		DelegatingResourceDescription description = super.getRepresentationDescription(rep);
 		description.removeProperty("name");
 		description.removeProperty("description");
@@ -26,32 +43,63 @@ public class InventoryStockTakeResource extends BaseRestMetadataResource<Invento
 		return description;
 	}
 
-	@PropertySetter(value = "operationNumber")
-	public void setOperationNumber(InventoryStockTake instance, String operationNumber) {
-		System.out.println("dddddddddddd");
-		instance.setOperationNumber(operationNumber);
+	public Boolean userCanProcess(StockOperation operation) {
+		return StockOperationTypeResource.userCanProcess(operation.getInstanceType());
 	}
 
-	@PropertySetter(value = "inventoryStockTakeList")
-	public void setInventoryStockTakeList(InventoryStockTake instance, List<InventoryStockTakeEntity> list) {
-		System.out.println("ffffffffffff");
-		instance.setInventoryStockTakeList(list);
-	}
-
-
-	@Override
-	public InventoryStockTake newDelegate() {
+	@Override public InventoryStockTake newDelegate() {
 		return new InventoryStockTake();
 	}
 
-	@Override
-	public InventoryStockTake save(InventoryStockTake delegate) {
-		System.out.println("lalalalalalal");
-		return delegate;
+	@Override public InventoryStockTake save(InventoryStockTake delegate) {
+		StockOperation operation = new StockOperation();
+		operation = createOperation(operation, delegate);
+		// Ensure that the current user can process the operation
+		if (!userCanProcess(operation)) {
+			throw new RestClientException("The current user not authorized to process this operation.");
+		}
+
+		InventoryStockTake inventoryStockTake = newDelegate();
+		operationService.submitOperation(operation);
+		//		// If the status has changed, submit the operation
+		//		stockOperationResource.save(operation);
+
+		return inventoryStockTake;
 	}
 
-	@Override
-    public Class<? extends IMetadataDataService<InventoryStockTake>> getServiceClass() {
-	    return null;
-    }
+	private StockOperation createOperation(StockOperation operation, InventoryStockTake delegate) {
+		operation.setStatus(StockOperationStatus.NEW);
+		IStockOperationType operationType = WellKnownOperationTypes.getAdjustment();
+		operation.setInstanceType(operationType);
+		Stockroom stockroom;
+		stockroom = stockroomDataService.getByUuid(delegate.getInventoryStockTakeList().get(0).getStockroom().getUuid());
+		operation.setSource(stockroom);
+		operation.setOperationNumber(delegate.getOperationNumber());
+		Date dNow = new Date();
+		operation.setOperationDate(dNow);
+		operation.setItems(createOperationsItemSet(operation, delegate.getInventoryStockTakeList()));
+		return operation;
+	}
+
+	private Set<StockOperationItem> createOperationsItemSet(StockOperation operation,
+			List<InventoryStockTakeEntity> stockTakeEntityList) {
+		Set<StockOperationItem> items = new HashSet<StockOperationItem>();
+		for (InventoryStockTakeEntity invitem : stockTakeEntityList) {
+			StockOperationItem item = new StockOperationItem();
+			item.setOperation(operation);
+			item.setItem(invitem.getItem());
+			item.setCalculatedExpiration(invitem.getCalculatedExpiration());
+			item.setExpiration(invitem.getExpiration());
+			if (invitem.getBatchOperation() == null) {
+				invitem.setBatchOperation(operation);
+				item.setBatchOperation(invitem.getBatchOperation());
+			}
+			int actualQuantity = invitem.getActualQuantity() - invitem.getQuantity();
+			item.setQuantity(actualQuantity);
+			item.setBatchOperation(operation);
+			items.add(item);
+		}
+		return items;
+	}
+
 }
