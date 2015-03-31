6fed4d5cb6769786bad88d9bf5bdf0dfff533cf2
Wes Brown <wes@weslandia.org>
2015-03-31 13:13:11 +0300

    Refactored resources to use updated stocktake page models.

--- omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeEntityResource.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeEntityResource.java
@@ -8,30 +8,32 @@ import org.openmrs.module.webservices.rest.web.annotation.Resource;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 
-@Resource(name = ModuleRestConstants.INVENTORY_STOCK_TAKE_ENTITY_RESOURCE, supportedClass = ItemStockSummary.class,
-supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" })
-public class InventoryStockTakeEntityResource extends ItemStockDetailBaseResource<ItemStockSummary> {
-
-    @Override
-    public DelegatingResourceDescription getRepresentationDescription(Representation rep)  {
-    	DelegatingResourceDescription description =  super.getRepresentationDescription(rep);
-		description.addProperty("stockroom", Representation.DEFAULT);
+@Resource(name = ModuleRestConstants.INVENTORY_STOCK_TAKE_SUMMARY_RESOURCE, supportedClass = ItemStockSummary.class,
+        supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" })
+public class InventoryStockTakeEntityResource extends BaseRestObjectResource<ItemStockSummary> {
+	@Override
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+		DelegatingResourceDescription description = super.getRepresentationDescription(rep);
+		description.addProperty("item", Representation.DEFAULT);
+		description.addProperty("expiration", Representation.DEFAULT);
+		description.addProperty("quantity", Representation.DEFAULT);
 		description.addProperty("actualQuantity", Representation.DEFAULT);
+		
 		return description;
-    }
-
-    @PropertySetter("expiration")
+	}
+	
+	@PropertySetter("expiration")
 	public void setExpiration(ItemStockSummary instance, String dateText) {
-    	System.out.println("aaaaaaaaaaaaaa");
-    }
-
+		System.out.println("aaaaaaaaaaaaaa");
+	}
+	
 	@Override
-    public Class<? extends IObjectDataService<ItemStockSummary>> getServiceClass() {
-	    return null;
-    }
-
+	public Class<? extends IObjectDataService<ItemStockSummary>> getServiceClass() {
+		return null;
+	}
+	
 	@Override
-    public ItemStockSummary newDelegate() {
-	    return new ItemStockSummary();
-    }
+	public ItemStockSummary newDelegate() {
+		return new ItemStockSummary();
+	}
 }
--- omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeResource.java
+++ omod/src/main/java/org/openmrs/module/webservices/rest/resource/InventoryStockTakeResource.java
@@ -2,9 +2,9 @@ package org.openmrs.module.webservices.rest.resource;
 
 import java.util.List;
 
-import org.openmrs.module.openhmis.commons.api.entity.IMetadataDataService;
-import org.openmrs.module.openhmis.inventory.api.model.InventoryStockTake;
+import org.openmrs.module.openhmis.commons.api.entity.IObjectDataService;
 import org.openmrs.module.openhmis.inventory.api.model.ItemStockSummary;
+import org.openmrs.module.openhmis.inventory.model.InventoryStockTake;
 import org.openmrs.module.openhmis.inventory.web.ModuleRestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.PropertySetter;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
@@ -12,46 +12,45 @@ import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
 
 @Resource(name = ModuleRestConstants.INVENTORY_STOCK_TAKE_RESOURCE, supportedClass = InventoryStockTake.class,
-supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" })
-public class InventoryStockTakeResource extends BaseRestMetadataResource<InventoryStockTake> {
-
+        supportedOpenmrsVersions = { "1.9.*", "1.10.*", "1.11.*" })
+public class InventoryStockTakeResource extends BaseRestObjectResource<InventoryStockTake> {
 	@Override
-	public DelegatingResourceDescription getRepresentationDescription(Representation rep)  {
+	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		DelegatingResourceDescription description = super.getRepresentationDescription(rep);
 		description.removeProperty("name");
 		description.removeProperty("description");
 		description.addProperty("operationNumber");
-		description.addProperty("inventoryStockTakeList");
-
+		description.addProperty("stockroom");
+		description.addProperty("itemStockSummaryList");
+		
 		return description;
 	}
-
+	
 	@PropertySetter(value = "operationNumber")
 	public void setOperationNumber(InventoryStockTake instance, String operationNumber) {
 		System.out.println("dddddddddddd");
 		instance.setOperationNumber(operationNumber);
 	}
-
-	@PropertySetter(value = "inventoryStockTakeList")
+	
+	@PropertySetter(value = "itemStockSummaryList")
 	public void setInventoryStockTakeList(InventoryStockTake instance, List<ItemStockSummary> list) {
 		System.out.println("ffffffffffff");
-		instance.setInventoryStockTakeList(list);
+		instance.setItemStockSummaryList(list);
 	}
-
-
+	
 	@Override
 	public InventoryStockTake newDelegate() {
 		return new InventoryStockTake();
 	}
-
+	
 	@Override
 	public InventoryStockTake save(InventoryStockTake delegate) {
 		System.out.println("lalalalalalal");
 		return delegate;
 	}
-
+	
 	@Override
-    public Class<? extends IMetadataDataService<InventoryStockTake>> getServiceClass() {
-	    return null;
-    }
+	public Class<? extends IObjectDataService<InventoryStockTake>> getServiceClass() {
+		return null;
+	}
 }
