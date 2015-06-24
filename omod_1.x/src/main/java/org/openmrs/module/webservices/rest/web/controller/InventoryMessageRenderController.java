62d2a404f4316fe76032bc97116a01ccc3ecc970
dubdabasoduba <dubdabasoduba@gmail.com>
2015-06-24 14:41:57 +0300

    edits

---
+++ omod_1.x/src/main/java/org/openmrs/module/webservices/rest/web/controller/InventoryMessageRenderController.java
@@ -0,0 +1,25 @@
+package org.openmrs.module.webservices.rest.web.controller;
+
+import org.openmrs.module.openhmis.backboneforms.web.BackboneWebConstants;
+import org.openmrs.module.openhmis.inventory.web.ModuleWebConstants;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created by dubdabasoduba on 16/04/15.
+ */
+@Controller @RequestMapping(ModuleWebConstants.MESSAGEPROPERTIES_JS_URI) public class InventoryMessageRenderController {
+
+	@RequestMapping(method = RequestMethod.GET) public ModelAndView MessageRenderController(HttpServletRequest request) {
+		Locale locale = RequestContextUtils.getLocale(request);
+		ResourceBundle resourceBundle = ResourceBundle.getBundle("messages", locale);
+		return new ModelAndView(ModuleWebConstants.MESSAGE_PAGE, "keys", resourceBundle.getKeys());
+	}
+}
--- omod_1.x/src/main/webapp/template/localHeader.jsp
+++ omod_1.x/src/main/webapp/template/localHeader.jsp
@@ -4,3 +4,4 @@
 <openmrs:htmlInclude file="<%= BackboneWebConstants.BACKBONE_INIT_JS %>" />
 <openmrs:htmlInclude file="<%= ModuleWebConstants.MODULE_INIT_JS %>" />
 <openmrs:htmlInclude file="<%= BackboneWebConstants.BACKBONE_CURL_JS %>" />
+<openmrs:htmlInclude file="/ws/module/openhmis/inventory/messageProperties.js" />
