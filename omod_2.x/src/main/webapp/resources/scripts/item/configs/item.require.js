8abab04d78f7133eb692a4c406acf7ffc217edc5
Andrew <andrewndungu@gmail.com>
2016-01-14 12:08:05 +0300

    Issue inv-282: Create OpenMRS 2-Compatible Item Page

---
+++ omod_2.x/src/main/webapp/resources/scripts/item/configs/item.require.js
@@ -0,0 +1,7 @@
+/* load institution modules */
+define(['item/models/item.model', 'reusable-components/services/restful-services/entity-rest.factory',
+        'item/services/item.functions', 'item/services/item.restful.services',
+        'reusable-components/controllers/generic.entity.controller.module',
+        'reusable-components/controllers/generic.entity.controller', 'item/controllers/item.controller',],
+    function() {
+    });
