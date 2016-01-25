8060527cced16595d16fbf7d70f98ce4cb03d206
Andrew <andrewndungu@gmail.com>
2016-01-25 09:28:41 +0300

    Moved header location to commons

--- omod_1.x/src/main/java/org/openmrs/module/openhmis/inventory/web/controller/Settings2xController.java
+++ omod_1.x/src/main/java/org/openmrs/module/openhmis/inventory/web/controller/Settings2xController.java
@@ -27,6 +27,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.openmrs.module.openhmis.commons.web.controller.HeaderController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
