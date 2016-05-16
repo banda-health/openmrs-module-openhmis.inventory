0f99a0761d6bccd13961b027899f3c4b39ea9716
Andrew <andrewndungu@gmail.com>
2016-05-16 13:00:26 +0300

    init commit

---
+++ omod_2.x/src/main/webapp/resources/scripts/createOperation/configs/entity.module.js
@@ -0,0 +1,67 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * Copyright (C) OpenHMIS.  All Rights Reserved.
+ *
+ */
+
+/*
+ * The module determines which page should be loaded depending on the url/route.
+ * The manage entities page loads all entities. The entity.page
+ * page either creates a new operation if NO uuid is given, else loads an
+ * existing entity for editing.
+ */
+(function() {
+    define([], loadpage);
+
+    function loadpage() {
+        'use strict';
+        var app = angular.module('stockOperationApp', ['ui.bootstrap', 'ngDialog', 'ui.router',
+            'angularUtils.directives.dirPagination', 'app.css', 'app.filters', 'app.pagination',
+            'app.cookies', 'app.genericMetadataModel', 'app.restfulServices',
+            'app.genericEntityController', 'app.genericManageController']);
+        app.config(function($stateProvider, $urlRouterProvider, $provide) {
+            /*
+             * Configure routes and urls. The default route is '/' which loads
+             * manage entities.page. 'edit' route calls entities.page -- it
+             * appends a 'uuid' to the url to edit an existing entity. 'new'
+             * route is called to create a new entity.
+             */
+            $urlRouterProvider.otherwise('/');
+            $stateProvider.state('/', {
+                url: '/',
+                templateUrl: 'entity.page',
+                controller: 'CreateStockOperationController'
+            });
+
+            $provide.factory('$exceptionHandler', function($injector) {
+                return function(exception, cause) {
+                    /*
+                     * Handle common exceptions
+                     */
+                    // unknown provider..
+                    var exc = String(exception);
+                    if (exc.indexOf("unpr") !== -1) {
+                        console.log("ISSUE LOADING MODULE(S)");
+                        console.log(exc);
+                    } else if (exc.indexOf("session") !== -1 || exc.indexOf("timeout") !== -1) {
+                        emr.message("SESSION TIMEOUT");
+                        console.log(exc + " - " + cause);
+                    } else {
+                        console.log(exc + " - " + cause);
+                        emr.message(cause);
+                    }
+                }
+            });
+        });
+        return app;
+    }
+})();
---
+++ omod_2.x/src/main/webapp/resources/scripts/createOperation/controllers/entity.controller.js
@@ -0,0 +1,134 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and
+ * limitations under the License.
+ *
+ * Copyright (C) OpenHMIS.  All Rights Reserved.
+ *
+ */
+
+(function() {
+    'use strict';
+
+    var base = angular.module('app.genericEntityController');
+    base.controller("CreateStockOperationController", CreateStockOperationController);
+    CreateStockOperationController.$inject = ['$stateParams', '$injector', '$scope', '$filter', 'EntityRestFactory',
+        'StockOperationModel', 'CreateStockOperationRestfulService', 'PaginationService', 'CreateStockOperationFunctions', 'CookiesService'];
+
+    function CreateStockOperationController($stateParams, $injector, $scope, $filter, EntityRestFactory, StockOperationModel, StockOperationRestfulService, PaginationService, StockOperationFunctions, CookiesService) {
+        var self = this;
+        var module_name = 'inventory';
+        var entity_name_message_key = emr.message("openhmis.inventory.stock.operation.name");
+        var cancel_page = 'entities.page';
+        var rest_entity_name = emr.message("openhmis.inventory.stock.operation.rest_name");
+
+        // @Override
+        self.setRequiredInitParameters = self.setRequiredInitParameters || function() {
+                self.bindBaseParameters(module_name, rest_entity_name, entity_name_message_key, cancel_page);
+            }
+
+        /**
+         * Initializes and binds any required variable and/or function specific to entity.page
+         * @type {Function}
+         */
+            // @Override
+        self.bindExtraVariablesToScope = self.bindExtraVariablesToScope
+            || function(uuid) {
+                // bind item stock variables/functions
+                $scope.stockOperationPagingFrom = PaginationService.pagingFrom;
+                $scope.stockOperationPagingTo = PaginationService.pagingTo;
+
+                $scope.stockOperationItemLimit = CookiesService.get(uuid + 'stockOperationItemLimit') || 5;
+                $scope.stockOperationItemCurrentPage = CookiesService.get(uuid + 'stockOperationItemCurrentPage') || 1;
+
+                // bind transaction variables/function
+                $scope.stockOperationTransactionLimit = CookiesService.get(uuid + 'stockOperationTransactionLimit')  || 5;
+                $scope.stockOperationTransactionCurrentPage = CookiesService.get(uuid + 'stockOperationTransactionCurrentPage') || 1;
+
+
+                $scope.stockOperationItem = self.stockOperationItem;
+                $scope.stockOperationTransaction = self.stockOperationTransaction;
+                $scope.invokeOperation = self.invokeOperation;
+                $scope.showOperationActionsDialog = StockOperationFunctions.showOperationActionsDialog;
+
+                self.stockOperation(uuid, rest_entity_name);
+                self.stockOperationItem(uuid, $scope.stockOperationItemCurrentPage);
+                self.stockOperationTransaction(uuid, $scope.stockOperationItemCurrentPage);
+
+            }
+
+        self.invokeOperation = self.invokeOperation || function(status, uuid){
+                StockOperationRestfulService.invokeOperation(status, uuid, rest_entity_name, self.onLoadInvokeOperationSuccessful);
+            }
+
+        /**
+         * @type {Function}
+         */
+        self.stockOperation = self.stockOperation || function(uuid, rest_entity_name){
+                StockOperationRestfulService.stockOperation(uuid, rest_entity_name, self.onLoadStockOperationSuccessful);
+            }
+
+        self.stockOperationItem = self.stockOperationItem || function(uuid, stockOperationItemCurrentPage){
+                stockOperationItemCurrentPage = stockOperationItemCurrentPage || $scope.stockOperationItemCurrentPage;
+                CookiesService.set(uuid + 'stockOperationItemCurrentPage', stockOperationItemCurrentPage);
+                CookiesService.set(uuid + 'stockOperationItemLimit', $scope.stockOperationItemLimit);
+
+                StockOperationRestfulService.stockOperationItem(
+                    uuid, CookiesService.get(uuid + 'stockOperationItemCurrentPage'),
+                    CookiesService.get(uuid + 'stockOperationItemLimit'),
+                    self.onLoadStockOperationItemSuccessful
+                );
+            }
+
+        self.stockOperationTransaction = self.stockOperationTransaction || function(uuid, stockOperationTransactionCurrentPage){
+                stockOperationTransactionCurrentPage = stockOperationTransactionCurrentPage || $scope.stockOperationTransactionCurrentPage;
+                CookiesService.set(uuid + 'stockOperationTransactionCurrentPage', stockOperationTransactionCurrentPage);
+                CookiesService.set(uuid + 'stockOperationTransactionLimit', $scope.stockOperationTransactionLimit);
+
+                StockOperationRestfulService.stockOperationTransaction(
+                    uuid, CookiesService.get(uuid + 'stockOperationTransactionCurrentPage'),
+                    CookiesService.get(uuid + 'stockOperationTransactionLimit'),
+                    self.onLoadStockOperationTransactionSuccessful
+                );
+            }
+
+        // callbacks
+        self.onLoadStockOperationSuccessful = self.onLoadStockOperationSuccessful || function(data){
+                $scope.stockOperation = data;
+            }
+
+        self.onLoadStockOperationItemSuccessful = self.onLoadStockOperationItemSuccessful || function(data){
+                $scope.stockOperationItems = data.results;
+                $scope.stockOperationItemTotalNumberOfResults = data.length;
+            }
+
+        self.onLoadStockOperationTransactionSuccessful = self.onLoadStockOperationTransactionSuccessful || function(data){
+                $scope.stockOperationTransactions = data.results;
+                $scope.stockOperationTransactionTotalNumberOfResults = data.length;
+            }
+
+        self.onLoadInvokeOperationSuccessful = self.onLoadInvokeOperationSuccessful || function(){
+                $scope.cancel();
+            }
+
+        // @Override
+        self.setAdditionalMessageLabels = self.setAdditionalMessageLabels || function(){
+                return StockOperationFunctions.addMessageLabels();
+            }
+
+        /* ENTRY POINT: Instantiate the base controller which loads the page */
+        $injector.invoke(base.GenericEntityController, self, {
+            $scope: $scope,
+            $filter: $filter,
+            $stateParams: $stateParams,
+            EntityRestFactory: EntityRestFactory,
+            GenericMetadataModel: StockOperationModel
+        });
+    }
+})();
