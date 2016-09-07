54117c86312636d42e0c59170af3fc4e528e73d2
Wes Brown <wes@weslandia.org>
2016-09-07 08:50:39 -0400

    Removed all content for teamcity dummy branch.

--- api/src/test/java/org/openmrs/module/openhmis/inventory/api/ITestableStockOperationService.java
+++
@@ -1,7 +0,0 @@
-package org.openmrs.module.openhmis.inventory.api;
-
-import org.openmrs.module.openhmis.inventory.api.model.StockOperation;
-
-public interface ITestableStockOperationService extends IStockOperationService {
-	void calculateReservations(StockOperation operation);
-}
--- omod/src/main/java/org/openmrs/module/openhmis/inventory/model/ItemStockEntry.java
+++
@@ -1,7 +0,0 @@
-package org.openmrs.module.openhmis.inventory.model;
-
-import org.openmrs.module.openhmis.inventory.api.model.ItemStockDetailBase;
-
-public class ItemStockEntry extends ItemStockDetailBase {
-	public static final long serialVersionUID = 0L;
-}
